4353ad4435e372244933080256cb1ad62154ed3c
kidusgituhb <kidus.addis@trojans.dsu.edu>
2020-03-03 13:40:45 -0600

    kf

---
+++ realcal.c
@@ -0,0 +1,1135 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}#include <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}lude <stdio.h>
+
+int main()
+
+{
+
+float y,x,result;
+
+int opp,result2,x2,y2;
+
+printf("Enter number 1 for addition: \n");
+
+printf("Enter number 2 for subtraction: \n");
+
+printf("Enter number 3 for multiplication: \n");
+
+printf("Enter number 4 for division: \n");
+
+printf("Enter number 5 for modules: \n");
+
+    printf("enter here ->> ");
+
+    scanf("%d", &opp);
+
+
+if(opp == 1)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y + x;
+    printf("%f\n",result);
+}
+
+else if(opp == 2)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y - x;
+    printf("%f\n",result);
+}
+
+else if(opp == 3)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y * x;
+    printf("%f\n",result);
+}
+
+else if(opp == 4)
+{
+    printf("first number: ");
+    scanf("%f", &y);
+    printf("second number: ");
+    scanf("%f", &x);
+    result = y / x;
+    printf("%f\n",result);
+}
+else if(opp == 5)
+{
+    printf("first number: ");
+    scanf("%d", &y2);
+    printf("second number: ");
+    scanf("%d", &x2);
+    result2 = y2 % x2;
+    printf("%d\n",result2);
+}
+
+else
+{
+    printf("enter correct opp\n");
+}
+
+return 0;
+
+}
